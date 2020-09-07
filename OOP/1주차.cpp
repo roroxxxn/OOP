536e3ee50a927aea4f945b647c18f030cd61851a
roroxxxn <63271398+roroxxxn@users.noreply.github.com>
2020-09-07 13:15:12 +0900

    초기 커밋.

---
+++ OOP/1주차.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+class LetDebug
+{
+public:
+    void printNum()
+    {
+        short s1 = 32767;
+        short s2 = 1;
+        int s3 = (short)s1 + s2;
+        std::cout << s3 << std::endl;
+    }
+};
+
+int main()
+{
+    LetDebug* ld = new LetDebug;
+    ld->printNum();
+
+    return 0;
+}
+
